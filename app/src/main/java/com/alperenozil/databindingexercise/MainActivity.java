bb391a90ccc3f816bec45d288d07594fe8718a30
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-10 07:02:21 +0300

    Student class is created. Textview values are coming from student object.

--- app/src/main/java/com/alperenozil/databindingexercise/MainActivity.java
+++ app/src/main/java/com/alperenozil/databindingexercise/MainActivity.java
@@ -3,12 +3,23 @@ package com.alperenozil.databindingexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textViewEmail,textViewName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewEmail=findViewById(R.id.textViewEmail);
+        textViewName=findViewById(R.id.textViewName);
+
+        textViewName.setText(getStudent().getName());
+        textViewEmail.setText(getStudent().getEmail());
+    }
+
+    public Student getStudent(){
+        Student student=new Student("alperen","mail");
+        return student;
     }
 }
